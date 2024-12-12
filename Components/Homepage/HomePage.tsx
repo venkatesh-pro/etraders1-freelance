161ec9ee4067936d1cba9f81ae7c5d7a5821e9b0
venkatesh k <programervenkatesh@gmail.com>
2024-12-12 16:34:58 +0530

    completed

--- Components/Homepage/HomePage.tsx
+++ Components/Homepage/HomePage.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Configurator from "./Configurator/Configurator";
 import { ConfiguratorData, data } from "@/data";
 import Slider from "./Slider/Slider";
 import usePreloadImages from "../../hooks/usePreloadImages";
+import { gsap } from "gsap/dist/gsap";
 
 type Model = { name: string };
 type Color = { name: string };
@@ -21,10 +22,10 @@ const HomePage = () => {
   const [isMirrored, setIsMirrored] = useState(false);
 
   const [sliderImages, setSliderImages] = useState([
-    "/ConfiguratorImages/BLACK COMPRESSED 16:25/16-black-1.jpg",
-    "/ConfiguratorImages/BLACK COMPRESSED 16:25/16-black-2.jpg",
-    "/ConfiguratorImages/BLACK COMPRESSED 16:25/16-black-3.jpg",
-    "/ConfiguratorImages/BLACK COMPRESSED 16:25/16-black-4.jpg",
+    // "/ConfiguratorImages/BLACK COMPRESSED 16:25/16-black-1.jpg",
+    // "/ConfiguratorImages/BLACK COMPRESSED 16:25/16-black-2.jpg",
+    // "/ConfiguratorImages/BLACK COMPRESSED 16:25/16-black-3.jpg",
+    // "/ConfiguratorImages/BLACK COMPRESSED 16:25/16-black-4.jpg",
   ]);
 
   const generateSliderImages = (
@@ -119,9 +120,35 @@ const HomePage = () => {
   const imagesLoaded = usePreloadImages(sliderImages);
   console.log({ imagesLoaded });
 
+  // useEffect(() => {
+  //   console.log("changed");
+  // }, [sliderImages]);
+
+  // Ref for the Loading Overlay
+  const loadingOverlayRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    console.log("changed");
-  }, [sliderImages]);
+    if (loadingOverlayRef.current) {
+      if (!imagesLoaded) {
+        // When images start loading, fade in the loading overlay
+        gsap.set(loadingOverlayRef.current, { display: "flex" }); // Ensure it's visible
+        gsap.to(loadingOverlayRef.current, {
+          opacity: 1,
+          duration: 0.2,
+        });
+      } else {
+        // When images are loaded, fade out the loading overlay
+        gsap.to(loadingOverlayRef.current, {
+          opacity: 0,
+          duration: 0.2,
+          onComplete: () => {
+            gsap.set(loadingOverlayRef.current, { display: "none" });
+          },
+        });
+      }
+    }
+  }, [imagesLoaded]);
+
   return (
     <div className="relative w-full max-h-[100vh] overflow-hidden">
       <Navbar />
@@ -129,13 +156,19 @@ const HomePage = () => {
       <div className="flex w-full h-screen md:h-[calc(100vh-50px)]">
         {/* Slider Section */}
         <div className="fixed md:static top-0 left-0 w-full md:w-[70%] h-[40vh] md:h-full overflow-hidden z-10 md:z-auto bg-white">
-          {!imagesLoaded ? (
-            <div className="flex items-center justify-center w-full h-full">
-              Loading images...
-            </div>
-          ) : (
+          <div className="relative w-full h-full">
             <Slider sliderImages={sliderImages} />
-          )}
+
+            {/* Loading Overlay */}
+            <div
+              ref={loadingOverlayRef}
+              className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-white"
+              style={{ opacity: 0, display: "none" }}
+            >
+              {/* Loader Animation */}
+              <div className="loader"></div>
+            </div>
+          </div>
         </div>
 
         {/* Configurator Section */}
